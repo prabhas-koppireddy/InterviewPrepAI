3440eea4875b9c82c7b0e0592a9f23c980d28dd4
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-07-25 12:15:47 +0530

    Initial commit

---
+++ frontend/interview-prep-ai/src/components/Cards/ProfileInfoCard.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import Avatar from "../../assets/hero-img.png"
+const ProfileInfoCard = () => {
+  const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/");
+  };
+// console.log(user);
+// console.log(user.profileImageUrl);
+  return user && (
+    <div className="flex items-center">
+      <img src={user?.profileImageUrl || Avatar} alt="Profile" className="w-11 h-11 bg-gray-300 rounded-full mr-3" />
+
+      <div>
+        <div className="text-[15px] text-black font-bold leading-3">{user.name || ""}</div>
+
+        <button className="text-amber-600 text-sm font-semibold cursor-pointer hover:underline" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfoCard;
