6a0f2075a4d1f6876fdf12d6680a85e91d0be323
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-07-26 11:29:55 +0530

    Project Successfully Completed

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);  
 });
